61d1254104f56227546e6839890b1fc757748e07
Alex <cigan@ua.fm>
2020-12-22 21:05:21 +0200

    Add POST and GET methods in fetch() and connect with Firebase

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -3,7 +3,9 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
       <ul>
         <survey-result
@@ -21,10 +23,39 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
+  //   props: ['results'],
   components: {
-    SurveyResult,
+    SurveyResult
   },
+  data() {
+    return {
+      results: []
+    };
+  },
+  methods: {
+    loadExperiences() {
+      // method POST - a default method that doesn't require header and body
+      fetch(
+        'https://vue-http-demo-30c54-default-rtdb.firebaseio.com/surveys.json'
+      )
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then(data => {
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating
+            });
+          }
+          this.results = results;
+        });
+    }
+  }
 };
 </script>
 
