69e2379af57c40707d6e4e9a1dc7067a70304789
Alex <cigan@ua.fm>
2020-12-23 14:47:16 +0200

    Catch a technical error and not using POST method

--- src/components/survey/LearningSurvey.vue
+++ src/components/survey/LearningSurvey.vue
@@ -41,6 +41,7 @@
         <p v-if="invalidInput">
           One or more input fields are invalid. Please check your provided data.
         </p>
+        <p v-if="error">Something went wrong: {{ error }}</p>
         <div>
           <base-button>Submit</base-button>
         </div>
@@ -55,7 +56,8 @@ export default {
     return {
       enteredName: '',
       chosenRating: null,
-      invalidInput: false
+      invalidInput: false,
+      error: null
     };
   },
   //   emits: ['survey-submit'],
@@ -74,6 +76,7 @@ export default {
       //   });
 
       //Для работы с Firebase адрес нужно дополнить 'surveys.json'
+      this.error = null;
       fetch(
         'https://vue-http-demo-30c54-default-rtdb.firebaseio.com/surveys.json',
         {
@@ -86,7 +89,18 @@ export default {
             rating: this.chosenRating
           })
         }
-      );
+      )
+        .then(response => {
+          if (response.ok) {
+            //...
+          } else {
+            throw new Error('Could not save data!');
+          }
+        })
+        .catch(error => {
+          console.log(error);
+          this.error = error;
+        });
 
       this.enteredName = '';
       this.chosenRating = null;
